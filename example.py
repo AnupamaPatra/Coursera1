10f38756452ecf3c5adac1716cc28b6648044cbc
AnupamaPatra <Patra.anupama04@gmail.com>
2023-01-05 14:47:22 +0000

    Hello there saving a new file

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()
